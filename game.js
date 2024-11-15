aca7d798d0fe29d57941469814f138a419124987
vLHv <vuluonghung2005@gmail.com>
2024-11-15 13:06:52 +0100

    Update game.js
    Code copy from Make your character move

--- game.js
+++ game.js
@@ -1,7 +1,137 @@
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(1280, 720);
 }
 
-function draw() {
-  background(255, 140, 0);
+// Character
+function character(x,y) {
+
+  // Cloud 
+  noStroke();
+  fill(0);
+  ellipse(x+40,y-30,100,100);
+  ellipse(x-10,y-45,100,100);
+  ellipse(x-60,y-30,110,100);
+  ellipse(x-90,y,120,120);
+  ellipse(x-70,y+20,110,120);
+  ellipse(x-5,y+30,120,100);
+  ellipse(x+60,y+20,120,100);
+  ellipse(x+55,y-10,120,100);
+  
+  //  Body 
+  noStroke(0);
+  fill(0);
+  ellipse(x-15,y-80,90,80);   
+  ellipse(x-20,y-110,80,80);
+  rect(x-60,y-110,5,40);
+  ellipse(x+4,y-120,40,40);
+  ellipse(x+12,y-75,50,120);
+  ellipse(x+15,y-65,50,120);
+
+  // Neck
+  fill(0,0,0);
+  ellipse(x-15,y-150,40,55);
+ 
+
+  // Head 
+  noStroke();
+  fill(0);
+  ellipse(x-15,y-190,80,80);
+  ellipse(x+2,y-175,60,50);
+  ellipse(x-28,y-208,50,55);
+  
+
+  // Hair 
+  fill(0);
+  beginShape();
+  vertex(x-30,y-220);
+  bezierVertex(x-30,y-220,x-35,y-280,x+10,y-300);
+  bezierVertex(x-10,y-220,x,y-220,x,y-220);
+  endShape();
+
+  beginShape();
+  vertex(x-10,y-240);
+  bezierVertex(x-10,y-240,x+20,y-270,x+50,y-260);
+  bezierVertex(x+50,y-260,x+20,y-240,x+20,y-190);
+  endShape();
+
+  beginShape();
+  vertex(x+20,y-220);
+  bezierVertex(x+20,y-220,x+40,y-240,x+60,y-220);
+  bezierVertex(x+50,y-220,x+30,y-210,x+20,y-190);
+  endShape();
+
+  beginShape();
+  vertex(x+20,y-220);
+  bezierVertex(x+20,y-210,x+30,y-215,x+50,y-205);
+  bezierVertex(x+40,y-200,x+20,y-190,x+20,y-170);
+  endShape();
+
+  beginShape();
+  vertex(x-50,y-210);
+  bezierVertex(x-50,y-210,x-85,y-210,x-100,y-190);
+  bezierVertex(x-50,y-200,x-55,y-180,x-55,y-190);
+  endShape();
+
+  beginShape();
+  vertex(x-50,y-200);
+  bezierVertex(x-50,y-200,x-80,y-180,x-100,y-165);
+  bezierVertex(x-40,y-165,x-40,y-160,x-40,y-160);
+  endShape();
+
+  beginShape();
+  vertex(x-35,y-180);
+  bezierVertex(x-35,y-180,x-70,y-165,x-85,y-150);
+  bezierVertex(x-30,y-160,x-40,y-160,x-40,y-160);
+  endShape();
+
+  // Tail 
+  noFill();
+  stroke(0);
+  strokeWeight(7);
+  bezier(x-140,y-180,x-140,y-90,x-70,y-90,x-50,y-60);
+
+  // Weapon
+  stroke(0);
+  strokeWeight(5);
+  line(x,y,x-80,y-220);
+ 
 }
+
+let x = 400;
+let y = -100;
+frameRate(60);
+
+// Character movement
+function draw() {
+
+  // Background and Position
+  background(120, 165, 200);
+  character(x,y);
+
+  // Movement stop at this position
+  if ( y <= 750) {
+      y = y + 5;
+  } 
+
+  // Character movement with arrow keys
+  if (keyCode === 38) {
+      y = y - 10;
+  } else if (keyCode === 40) {
+      y = y + 0;
+  }
+
+  if (keyCode === 37) {
+      x = x - 5;
+  } else if (keyCode === 39) {
+      x = x + 5;
+  }   
+}
+
+// Spacebar key to reset character position
+function keyPressed()  {
+  if (keyCode === 32) {
+      y = -150;
+      x = 400;
+  }
+}
+
